8a3fe651cb6cc48350f55c50f9ccde300e19133e
Zahid <thezaza102@gmail.com>
2021-03-19 23:17:09 +1100

    intersection

--- src/mOps/sets/operations/operations.py
+++ src/mOps/sets/operations/operations.py
@@ -9,3 +9,13 @@ def union (*args, sort=True):
         raise TypeError('Set must be iterable')
         
 U=union
+
+def intersection(*args):
+    if all([hasattr(a, '__iter__') for a in args]):
+        result = list(set.intersection(*map(set,args)))  
+        result.sort()
+        return result
+    else:
+        raise TypeError('Set must be iterable')
+
+I = intersection
--- src/tests/test_set_operations.py
+++ src/tests/test_set_operations.py
@@ -3,6 +3,11 @@ import mOps.sets.operations.operations as ms
 
 
 class TestSetOperations(unittest.TestCase):
+
+    def test_function_alias(self):
+        self.assertEqual(ms.U, ms.union)
+        self.assertEqual(ms.I, ms.intersection)
+
     def test_union_sorted(self):
         a = [0,2,4,6,8]
         b = [1,3,5,7,9]
@@ -18,11 +23,26 @@ class TestSetOperations(unittest.TestCase):
         self.assertListEqual(aResult,result)
 
     def test_union_non_iter(self):
-        a = "02468"
+        a = 0
         b = [1,3,5,7,9]
         try:
             ms.union(a,b)
             self.fail('Set must be iterable')
         except TypeError:
             self.assertTrue(True)
+
+    def test_intersection(self):
+        a = [0,2,4,6,8,10,12]
+        b = [0,3,6,9,12]
+        result = ms.intersection(a,b)
+        aResult = [0,6,12]
+        self.assertListEqual(aResult,result)
             
+    def test_intersection_non_iter(self):
+        a = 0
+        b = [1,3,5,7,9]
+        try:
+            ms.intersection(a,b)
+            self.fail('Set must be iterable')
+        except TypeError:
+            self.assertTrue(True)
